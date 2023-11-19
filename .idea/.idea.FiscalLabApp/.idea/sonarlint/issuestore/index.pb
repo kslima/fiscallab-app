e3c1a25fe58a6455cdfa7a1ccc24baf71e14292f
Kleuder Lima <kleuder.lima@cedrotech.com>
2023-11-19 10:16:34 -0300

    add desintegrator pobre page

---
+++ .idea/.idea.FiscalLabApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,27 @@
+
+a
+1FiscalLabApp/wwwroot/sample-data/visit-pages.json,d\6\d6ff6055a659d329f0f14acceed2ddd74c1e794f
+_
+/FiscalLabApp/Extensions/MenuOptionExtensions.cs,a\c\acc8b2e44fdead5cc492692742f142443a36052e
+O
+FiscalLabApp/Models/PageType.cs,c\0\c0db9431852195a60af40f11d416167c9cc76598
+Z
+*FiscalLabApp/Components/Pages/Visits.razor,5\1\51886b75757b318197cfa7837aa4d0ef940127e1
+f
+6FiscalLabApp/Components/Pages/VisitDataSeparator.razor,2\7\274b67b15a77de05d747adb3d39928ec1433c0ee
+]
+-FiscalLabApp/Components/Pages/MainVisit.razor,f\4\f4d4007e0175b729729f190796c88f7ccd3a1465
+]
+-FiscalLabApp/Components/Pages/CaneScale.razor,3\f\3f954154ce948437e4dde8368c8ce756f84987f7
+]
+-FiscalLabApp/wwwroot/sample-data/options.json,0\5\050ed4270e164b54ed7f6758b765a0638bdf250e
+\
+,FiscalLabApp/Components/Layout/NavMenu.razor,9\b\9b3fe5fcc824654a889449af604e6dc32627282c
+O
+FiscalLabApp/wwwroot/index.html,e\8\e880b422c9ce2934a17023d690fd2346f3982a8b
+O
+FiscalLabApp/Models/MenuType.cs,8\f\8f61d1856971b1c863148c68f58ead89823720c7
+L
+FiscalLabApp/Models/Visit.cs,e\4\e4564006d36936865b6a9f28a0d9e4a885359e3a
+f
+6FiscalLabApp/Components/Pages/DesintegratorProbe.razor,a\4\a4cf3acff2590ca950495ff2cf39205e8303096b
--- FiscalLabApp/Models/Visit.cs
+++ FiscalLabApp/Models/Visit.cs
@@ -29,4 +29,30 @@ public class Visit
     public string PlantFarmPercentage { get; set; } = string.Empty;
     public string FarmProviderPercentage { get; set; } = string.Empty;
     public string Observations2 { get; set; } = string.Empty;
+    
+    
+    //Desintegrador Probe
+    public string ProbeTYpe { get; set; } = string.Empty;
+    public string TubeInserted { get; set; } = string.Empty;
+    public string TubeDischarged { get; set; } = string.Empty;
+    public string Collect { get; set; } = string.Empty;
+    public string SampleAmount { get; set; } = string.Empty;
+    public string BrothExtraction { get; set; } = string.Empty;
+    public string LoadPosition { get; set; } = string.Empty;
+    public string ToothedCrown { get; set; } = string.Empty;
+    public string ToothedCrownType { get; set; } = string.Empty;
+    public string LastCrownChange { get; set; } = string.Empty;
+    public string Observations3 { get; set; } = string.Empty;
+    public string HomogeneousSamples { get; set; } = string.Empty;
+    public string KnifeConservation { get; set; } = string.Empty;
+    public string AgainstKnifeConservation { get; set; } = string.Empty;
+    public string KnifeAgainstKnifeDistance { get; set; } = string.Empty;
+    public string HammerConservation  { get; set; } = string.Empty;
+    public string CleanMixer  { get; set; } = string.Empty;
+    public string DesintegratorRpm  { get; set; } = string.Empty;
+    public string PreparationIndex  { get; set; } = string.Empty;
+    public string LastRazorChange  { get; set; } = string.Empty;
+    public string LastHammerChange  { get; set; } = string.Empty;
+    public string LastAgainstKnifeChange  { get; set; } = string.Empty;
+    public string Observations4 { get; set; } = string.Empty;
 }
