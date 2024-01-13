6815b030e08f5ddf1efdb3b949d3519b812ff64e
Kleuder lima <kleuder.lima@cedrotech.com>
2024-01-13 14:36:37 -0300

    add pdf

---
+++ FiscalLabApp/wwwroot/js/interop.js
@@ -0,0 +1,20 @@
+// wwwroot/interop.js
+window.saveAsFile = function (fileName, content) {
+    const blob = base64toBlob(content);
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
+
+function base64toBlob(base64) {
+    const binaryString = window.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return new Blob([bytes]);
+}
