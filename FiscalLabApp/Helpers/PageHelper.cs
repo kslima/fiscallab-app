b965aa2734d61f268d837e083600b02239a36a7e
Kleuder lima <kleuder.lima@cedrotech.com>
2024-03-25 04:28:43 -0300

    .\%USERPROFILE%\.aspnet\https\aspnetapp.pfx

--- FiscalLabApp/Helpers/PageHelper.cs
+++ FiscalLabApp/Helpers/PageHelper.cs
@@ -4,7 +4,7 @@ public static class PageHelper
 {
     public const string BasicInformationPageName = "main";
     public const string SugarcaneBalancePageName = "sugarcane-balance";
-    public const string DesintegratorProbePageName = "desintegrator-probee";
+    public const string DesintegratorProbePageName = "desintegrator-probe";
     public const string AnalyticalBalancePageName = "analytical-balance";
     public const string PressRefractometerPageName = "press-refractometer";
     public const string ClarificationSaccharimeterPageName = "clarification-saccharimeter";
